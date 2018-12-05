b56331a61bf7881d241baf6234055db26a77d998
Obi Nkwonta <obi136@gmail.com>
2018-12-05 16:31:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var katzDeliLine = [];
-function takeANumber(katzDeliLine, customerName){
-  katzDeliLine.push(`${customerName}`);
-  return(`Welcome, ${customerName}. You are number ${katzDeliLine.length} in line.`)
+var counter = 0;
+
+function takeANumber(katzDeliLine) {
+  counter++
+  katzDeliLine.push(counter)
+  return(`Welcome you are number ${counter}.`)
 }
+
+console.log(takeANumber(katzDeliLine))
+console.log(takeANumber(katzDeliLine))
+console.log(takeANumber(katzDeliLine))
 
 function nowServing(katzDeliLine){
   if(!katzDeliLine.length){
@@ -13,22 +20,25 @@ function nowServing(katzDeliLine){
   //   {
   //     return "There is nobody waiting to be served!";
   //   }
-  else {
+  //  else {
     return(`Currently serving ${katzDeliLine.shift()}.`);
-  }
+  //  }
 }
 
-var line = [];
 
 function currentLine(katzDeliLine){
+  var line = []
   if(!katzDeliLine.length){
     return "The line is currently empty."
   }
 
   // var line = [];
   for (var i = 0; i < katzDeliLine.length; i++){
-    line.push(` `+[i+1]+`. ` + katzDeliLine[i]);
+    line.push(` `+ (i+1) +`. ` + katzDeliLine[i]);
 
   }
   return (`The line is currently:` + line);
 }
+
+console.log(currentLine(["Obi", "Jess", "Andrew"]))
+console.log(currentLine(["Steven", "Alex", "Vanessa"]))
